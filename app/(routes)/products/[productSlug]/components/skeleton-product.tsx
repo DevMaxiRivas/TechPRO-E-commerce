5a4d8065ce2e604562a0deb1d420d5327249695b
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-12 17:00:27 -0300

    fix(component): Design Skeleton have been modified

--- app/(routes)/products/[productSlug]/components/skeleton-product.tsx
+++ app/(routes)/products/[productSlug]/components/skeleton-product.tsx
@@ -2,15 +2,16 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SkeletonProduct = () => {
     return (
-        <div className="grid sm:grid-cols-2 sm:py-16 sm:px-40">
-            <Skeleton className="h-[200px] w-[350px] rounded-xl" />
-            <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]" />
-                <Skeleton className="h-4 w-[200px]" />
-                <Skeleton className="h-4 w-[200px]" />
-                <Skeleton className="h-4 w-[200px]" />
+        <div className="grid grid-cols-2 grid-rows-1 gap-4">
+            <div className="">
+                <Skeleton className="px-5 h-50 w-full rounded-xl" />
             </div>
-
+            <div className="sm:px-12 space-y-2">
+                <Skeleton className="h-4 w-55" />
+                <Skeleton className="h-4 w-50" />
+                <Skeleton className="h-4 w-50" />
+                <Skeleton className="h-4 w-50" />
+            </div>
         </div>
     );
 }
